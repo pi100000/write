7ae360622e8c8a06a0a421da1ec516ceffa4da56
pi100000 <pinak001@protonmail.com>
2023-09-24 11:38:53 +0530

    base: config and setup for postgresql

---
+++ backend/write-service/src/database/database.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { Pool } from "pg";
+
+@Injectable()
+export class DatabaseService {
+  private readonly pool: Pool;
+
+  constructor() {
+    this.pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+  }
+
+  async query(queryString: string, params: any[] = []): Promise<any> {
+    const client = await this.pool.connect();
+    try {
+      const result = await client.query(queryString, params);
+      return result;
+    } finally {
+      client.release();
+    }
+  }
+}
