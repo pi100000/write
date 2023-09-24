40049fa5e512a1fa19ae990b2a95c0210cdbf998
pi100000 <pinak001@protonmail.com>
2023-09-24 11:47:39 +0530

    fix(write-service): moved logic to app.service

--- backend/write-service/src/app.controller.ts
+++ backend/write-service/src/app.controller.ts
@@ -1,20 +1,16 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
-import { DatabaseService } from "./database/database.service";
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly databaseService: DatabaseService
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get("/test")
   async findAll(): Promise<any[]> {
     const query = "SELECT * FROM table1";
 
     try {
-      const data = await this.databaseService.query(query);
+      const data = await this.appService.query(query);
       return data;
     } catch (error) {
       throw error;
