bd03b449bac49b95d4f6c0e02ab2ffedda411a2a
pi100000 <pinak001@protonmail.com>
2023-10-07 16:13:25 +0530

    feat(write-service): update api + dtos

--- backend/write-service/src/app.controller.ts
+++ backend/write-service/src/app.controller.ts
@@ -1,6 +1,18 @@
-import { Body, Controller, Get, Post, Query } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from "@nestjs/common";
 import { AppService } from "./app.service";
 import { Write } from "./models/write.model";
+import { createWriteDto } from "dto/create-write.dto";
+import { updateWriteDto } from "dto/update-write.dto";
 
 @Controller()
 export class AppController {
@@ -19,8 +31,17 @@ export class AppController {
   }
 
   @Post("/write")
-  async create(@Body() writeData: Write): Promise<Write> {
+  async create(@Body() writeData: createWriteDto): Promise<createWriteDto> {
     const create = await this.appService.create(writeData);
     return create;
+  }
+
+  @Put("/update")
+  async update(
+    @Query("id") id: number,
+    @Body() updateWriteDto: updateWriteDto
+  ): Promise<Write> {
+    const updatedData = await this.appService.updateOne(id, updateWriteDto);
+    return updatedData;
   }
 }
--- backend/write-service/src/app.service.ts
+++ backend/write-service/src/app.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { Pool } from "pg";
 import { Write } from "./models/write.model";
+import { createWriteDto } from "dto/create-write.dto";
+import { updateWriteDto } from "dto/update-write.dto";
 
 @Injectable()
 export class AppService {
@@ -15,9 +17,10 @@ export class AppService {
   async getOne(id: number): Promise<Write | null> {
     const client = await this.pool.connect();
     try {
-      const { rows } = await client.query("SELECT * FROM write WHERE id = $1", [
-        id,
-      ]);
+      const { rows } = await client.query({
+        text: `SELECT * FROM write WHERE id = $1`,
+        values: [id],
+      });
       return rows[0] || null;
     } finally {
       client.release();
@@ -34,14 +37,45 @@ export class AppService {
     }
   }
 
-  async create(write: Write): Promise<Write> {
+  async create(write: createWriteDto): Promise<createWriteDto> {
     const client = await this.pool.connect();
     try {
-      const { rows } = await client.query(
-        "INSERT INTO write(title, content, tags) VALUES($1, $2, $3) RETURNING *",
-        [write.title, write.content, write.tags]
-      );
+      const { rows } = await client.query({
+        text: `INSERT INTO write(title, content, tags) VALUES($1, $2, $3) RETURNING *`,
+        values: [write.title, write.content, write.tags],
+      });
       return rows[0];
+    } finally {
+      client.release();
+    }
+  }
+
+  async updateOne(
+    id: number,
+    updateWriteDto: updateWriteDto
+  ): Promise<Write | null> {
+    const client = await this.pool.connect();
+    try {
+      const query = {
+        text: `
+          UPDATE write
+          SET
+            title = COALESCE($2, title),
+            content = COALESCE($3, content),
+            tags = COALESCE($4, tags)
+          WHERE id = $1
+          RETURNING *
+          `,
+        values: [
+          id,
+          updateWriteDto.title,
+          updateWriteDto.content,
+          updateWriteDto.tags,
+        ],
+      };
+
+      const { rows } = await client.query(query);
+      return rows[0] || null;
     } finally {
       client.release();
     }
