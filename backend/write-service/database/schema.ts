fa58abbbe7f3ec2aac97c63adc8b1781a1958c2d
pi100000 <pinak001@protonmail.com>
2025-07-03 23:11:02 +0530

    fix: write schema minor addition

--- backend/write-service/database/schema.ts
+++ backend/write-service/database/schema.ts
@@ -1,8 +1,8 @@
 import { integer, pgTable, text, varchar } from "drizzle-orm/pg-core";
 
 export const writeTable = pgTable("write", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  title: varchar({ length: 255 }).notNull(),
-  content: text(),
-  tags: varchar().array(),
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar("title", { length: 255 }).notNull(),
+  content: text("content"),
+  tags: varchar("tags").array(),
 });
